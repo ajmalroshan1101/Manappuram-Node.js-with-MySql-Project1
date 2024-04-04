657621ae003b08be2a8510d41da0f5727cf7838b
ajmalroshan1101 <ajumalroshan17@gmail.com>
2024-04-04 11:24:59 +0530

    Updation

--- utility/sqldb.js
+++ utility/sqldb.js
@@ -1,10 +1,20 @@
 const sql = require('mysql');
 
 const connection = sql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'db_2'
+    host: 'manpuramproddb.cd2ia2mk2bmm.ap-south-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'QBX1gbx5WQAzS32KjBya',
+    database: 'db_audit_tracker'
 });
 
 module.exports = connection;
+//  host: 'manpuramproddb.cd2ia2mk2bmm.ap-south-1.rds.amazonaws.com',
+// user: 'admin',
+// password: 'QBX1gbx5WQAzS32KjBya',
+// password: 'QBX1gbx5WQAzS32KjBya',
+// database: 'db_audit_tracker'
+
+// host: 'localhost',
+// user: 'root',
+// password: '',
+// database: 'db_2'
